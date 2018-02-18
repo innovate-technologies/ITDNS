3dd779127dc3b87e7bc66ba32889085b1e43f139
Maartje Eyskens <meyskens@me.com>
2018-02-18 15:28:02 +0100

    vendor

--- etcd3/etcd3.go
+++ etcd3/etcd3.go
@@ -7,9 +7,9 @@ import (
 	"strings"
 	"time"
 
-	etcd "gopkg.in/coreos/etcd.v3/clientv3"
-	"gopkg.in/coreos/etcd.v3/mvcc/mvccpb"
-	"gopkg.in/coreos/etcd.v3/pkg/transport"
+	etcd "github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/mvcc/mvccpb"
+	"github.com/coreos/etcd/pkg/transport"
 
 	"github.com/innovate-technologies/ITDNS/cache"
 	"github.com/innovate-technologies/ITDNS/config"
